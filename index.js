1c9bbaf8ed483c0795e1ee3d70ab5962e217c691
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-18 20:29:08 +0000

    walking through exercises again

--- index.js
+++ index.js
@@ -16,3 +16,22 @@ if (engineIndicatorLight === "green") {
   console.log("engines are off");
 }
 
+if (crewStatus) {
+  console.log("Crew Ready");
+} else console.log("Crew Not Ready");
+
+if (computerStatusCode === 200) {
+  console.log("Please stand by. Computer is rebooting.");
+} else if (computerStatusCode == 400){
+  console.log("Success! Computer online.");
+} else{ console.log("ALERT: Computer offline!");
+}
+
+
+
+if (shuttleSpeed > 17500) {
+  console.log("ALERT: Escape velocity reached!");
+} else if (shuttleSpeed < 8000){
+  console.log("ALERT: Cannot maintain orbit!");
+} else{ console.log("Stable speed!");
+}
